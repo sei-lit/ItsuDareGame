6244f0bc35862ad16e2c229eb6edeb1661b136f0
SeiOmori <sei.omori.22@gmail.com>
2023-05-11 16:41:25 +0900

    Initial Commit

---
+++ ItsuDareGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ItsuDareGame
+//
+//  Created by 大森青 on 2023/05/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
